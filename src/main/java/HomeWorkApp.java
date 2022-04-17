36d99ffb5f7bf6bd27e0eb84acfe9b478e5c7d7c
lewade <lewade>
2022-04-17 12:44:02 +0300

    upd branch2

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,2 +1,29 @@
+
 public class HomeWorkApp {
+    public static void main(String[] args) {
+        String[][] arr = {{"1", "1", "1", "1"},{"2", "2", "2", "2"},{"3", "3", "3", "3"},{"4", "4", "4", "4"}};
+        arrChange(arr);
+
+    }
+
+    public static void arrChange(String[][] array) {
+        int res = 0;
+        for (int i = 0; i < array.length; i++) {
+            res += array[i].length;
+        }
+        if(res != 16) {
+            throw new MyArraySizeException("Не соответствует размеру");
+        }else {
+            System.out.println(res);
+        }
+
+        int sumOfArr = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sumOfArr += Integer.parseInt(array[i][j]);
+            }
+        }
+        System.out.println(sumOfArr);
+
+    }
 }
---
+++ src/main/java/MyArraySizeException.java
@@ -0,0 +1,5 @@
+public class MyArraySizeException extends IndexOutOfBoundsException{
+    public MyArraySizeException(String s) {
+        super(s);
+    }
+}
