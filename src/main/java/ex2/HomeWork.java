5020e3d2c68a6126e715f5d04d06f5b12fd229a6
avNem <lewadebeats@gmail.com>
2022-04-17 16:15:28 +0300

    updt br3

---
+++ src/main/java/ex2/HomeWork.java
@@ -0,0 +1,12 @@
+package ex2;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        PhoneBook.add(13, "Ivanov");
+        PhoneBook.add(14, "Ivanov");
+        PhoneBook.add(15,"Vasya");
+
+
+        PhoneBook.get("Ivanov");
+    }
+}
