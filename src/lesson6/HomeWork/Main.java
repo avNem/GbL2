14a136c371a3f9902fe6f3406b878ed3afd61525
lewade <avnemtcev@mail.ru>
2022-04-27 22:37:16 +0300

    update

--- src/lesson6/HomeWork/Main.java
+++
@@ -1,9 +0,0 @@
-package lesson6.HomeWork;
-
-public class Main {
-    public static void main(String[] args) {
-        Client c1 = new Client("127.0.0.1", 8189);
-        c1.connectToServer("Ну здарова");
-
-    }
-}
